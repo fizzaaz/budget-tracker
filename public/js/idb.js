a4b4afa926102feadb7f6b3277fa740abb8125df
unknown <fizz.zehra14@gmail.com>
2021-09-22 12:52:06 -0500

    creating idbs

--- public/js/idb.js
+++ public/js/idb.js
@@ -35,8 +35,54 @@ function saveRecord(record) {
     const transaction = db.transaction(['pending'], 'readwrite');
   
     // access the object store for `pending`
-    const budgerObjectStore = transaction.objectStore('pending');
+    const budgetObjectStore = transaction.objectStore('pending');
   
     // add record to your store with add method
-    budgerObjectStore.add(record);
+    budgetObjectStore.add(record);
+  }
+
+
+  function uploadBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['pending'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('pending');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // more to come...
+
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['pending'], 'readwrite');
+          // access the pending object store
+          const budgetObjectStore = transaction.objectStore('pending');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved pending amount has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
   }
