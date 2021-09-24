68efc6719ac3f117f522293ef684e9d69223c076
unknown <fizz.zehra14@gmail.com>
2021-09-23 19:58:19 -0500

    trying to fix issues

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const APP_PREFIX = 'BudgetTracker-';
+const APP_PREFIX = 'Budget-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
 	'icons/icon-72x72.png',
 	'icons/icon-96x96.png',
 	'icons/icon-128x128.png',
-'icons/icon-144x144.png',
+  'icons/icon-144x144.png',
 	'icons/icon-152x152.png',
 	'icons/icon-192x192.png',
 	'icons/icon-384x384.png',
